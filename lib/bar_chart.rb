583a197c1f0a216e8e3d24a516328fc2c73cfce9
msalzburg <salzburg@emjot.de>
2010-07-08 18:13:15 +0200

    * check attributes for area, bar, line and pie chart

--- lib/bar_chart.rb
+++ lib/bar_chart.rb
@@ -2,37 +2,38 @@ module GoogleVisualr
 
   # http://code.google.com/apis/visualization/documentation/gallery/barchart.html
   class BarChart < BaseChart
+    # NOTE: attributes checked
 
     attr_accessor :element_id
 
     # http://code.google.com/apis/visualization/documentation/gallery/barchart.html#Configuration_Options
-    attr_accessor :axisColor
-    attr_accessor :axisBackgroundColor
-    attr_accessor :axisFontSize
+    # attr_accessor :axisColor
+    # attr_accessor :axisBackgroundColor
+    # attr_accessor :axisFontSize
     attr_accessor :backgroundColor
-    attr_accessor :borderColor
+    # attr_accessor :borderColor
     attr_accessor :colors
-    attr_accessor :enableTooltip
-    attr_accessor :focusBorderColor
+    attr_accessor :fontSize
+    # attr_accessor :enableTooltip
+    # attr_accessor :focusBorderColor
+    attr_accessor :hAxis
     attr_accessor :height
-    attr_accessor :is3D
+    # attr_accessor :is3D
     attr_accessor :isStacked
     attr_accessor :legend
-    attr_accessor :legendBackgroundColor
+    # attr_accessor :legendBackgroundColor
     attr_accessor :legendFontSize
     attr_accessor :legendTextColor
     attr_accessor :reverseCategories
-    attr_accessor :showCategories
+    # attr_accessor :showCategories
     attr_accessor :title
     attr_accessor :titleColor
     attr_accessor :titleFontSize
     attr_accessor :tooltipFontSize
-    attr_accessor :tooltipHeight
-    attr_accessor :tooltipWidth
-    attr_accessor :width
-        
+    # attr_accessor :tooltipHeight
+    # attr_accessor :tooltipWidth
     attr_accessor :vAxis
-    attr_accessor :hAxis
+    attr_accessor :width
 
     def render (element_id)
 
