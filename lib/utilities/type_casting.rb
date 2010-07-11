333174275511a37f0623c24f0b093d6ebb005b91
msalzburg <salzburg@emjot.de>
2010-07-11 15:24:00 +0200

    * code cleaning

--- lib/utilities/type_casting.rb
+++ lib/utilities/type_casting.rb
@@ -9,11 +9,12 @@ module GoogleVisualr
         # If the column type is 'boolean'   , the value should be a boolean.
         # If the column type is 'date'      , the value should be a Date object.
         # If the column type is 'datetime'  , the value should be a DateTime or Time object.
+        # If the column type is 'hash'      , the value should be a javascript hash.
         # If the column type is 'timeofday' , the value should be an array of three or four numbers: [hour, minute, second, optional milliseconds].
 
         case
           when value.is_a?(String)
-            return "'#{GoogleVisualr::Utilities::TypeCasting.escape_single_quotes(value)}'"
+            return "'#{value.gsub(/[']/, '\\\\\'')}'"
           when value.is_a?(Integer)   || value.is_a?(Float)
             return value
           when value.is_a?(TrueClass) || value.is_a?(FalseClass)
@@ -31,13 +32,8 @@ module GoogleVisualr
           else
             return value
         end
-
       end
-      
-      def self.escape_single_quotes(str)
-        return str.gsub(/[']/, '\\\\\'')
-      end
-    
+          
     end
     
   end
